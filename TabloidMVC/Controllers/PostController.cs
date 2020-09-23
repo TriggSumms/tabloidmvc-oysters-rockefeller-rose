4cfe7ecf03996cb71e5ca5d0f74a45fc432b9d16
Tasha Lane <designtmg77@gmail.com>
2020-09-23 16:31:52 -0500

    initial post edit

--- TabloidMVC/Controllers/PostController.cs
+++ TabloidMVC/Controllers/PostController.cs
@@ -2,7 +2,10 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
 using System.Security.Claims;
+using TabloidMVC.Models;
 using TabloidMVC.Models.ViewModels;
 using TabloidMVC.Repositories;
 
@@ -13,6 +16,7 @@ namespace TabloidMVC.Controllers
     {
         private readonly IPostRepository _postRepository;
         private readonly ICategoryRepository _categoryRepository;
+        private int userId;
 
         public PostController(IPostRepository postRepository, ICategoryRepository categoryRepository)
         {
@@ -41,6 +45,8 @@ namespace TabloidMVC.Controllers
             return View(post);
         }
 
+        
+
         public IActionResult Create()
         {
             var vm = new PostCreateViewModel();
@@ -66,6 +72,85 @@ namespace TabloidMVC.Controllers
                 vm.CategoryOptions = _categoryRepository.GetAll();
                 return View(vm);
             }
+        }
+
+        // GET: PostController/Edit/5
+        public IActionResult Edit(int id)
+        {
+            Post post = _postRepository.GetPublishedPostById(id);
+            int userId = GetCurrentUserProfileId();
+            var categoryOptions = _categoryRepository.GetAll();
+
+            if (post == null)
+            {
+                return NotFound();
+            }
+
+            var vm = new PostCreateViewModel()
+            {
+
+
+
+                Post = post,
+                CategoryOptions = categoryOptions
+            };
+
+            return View(vm);
+        }
+
+        // POST: PostController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Post post)
+        {
+            try
+            {
+                _postRepository.UpdatePost(post);
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                PostCreateViewModel vm = new PostCreateViewModel()
+
+                {
+
+                    Post = post
+                };
+
+                return View(post);
+            }
+        }
+
+
+        // GET: Post/Delete/5
+        public ActionResult Delete(int id)
+        {
+            int userId = GetCurrentUserProfileId();
+            Post post = _postRepository.GetUserPostById(id, userId);
+        
+                
+
+            return View(post);
+        }
+
+        // POST: PostController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, Post post)
+        {
+            try
+            {
+                _postRepository.DeletePost(id);
+                return RedirectToAction("Index");
+            }
+            catch (Exception)
+            {
+                return View(post);
+            }
+        }
+        public ActionResult Login()
+        {
+            return View();
         }
 
         private int GetCurrentUserProfileId()
