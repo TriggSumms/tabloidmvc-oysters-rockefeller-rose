d87f150ef41bab01750223fd6be6f6f5d2d04ac0
Chad Jones <chadjones839@gmail.com>
2020-09-25 13:36:52 -0500

    Edit button conditionals

--- TabloidMVC/Models/ViewModels/CommentViewModel.cs
+++ TabloidMVC/Models/ViewModels/CommentViewModel.cs
@@ -8,6 +8,7 @@ namespace TabloidMVC.Models.ViewModels
     public class CommentViewModel
     {
         public Post Post { get; set; }
+        public UserProfile User { get; set; }
         public List<Comment> Comments { get; set; }
     }
 }
